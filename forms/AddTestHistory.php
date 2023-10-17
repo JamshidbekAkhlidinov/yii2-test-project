da7dc848f9cd9cb7a171ca18c1b54b987813ab8e
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2023-10-17 13:56:55 +0500

    add new form. form is cheking test and add history

---
+++ forms/AddTestHistory.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *   Jamshidbek Akhlidinov
+ *   16 - 10 2023 18:51:25
+ *   https://github.com/JamshidbekAkhlidinov
+ */
+
+namespace app\forms;
+
+use app\models\HistoryOfSolution;
+use yii\base\Model;
+
+class AddTestHistory extends Model
+{
+    public $subject_id;
+    public $test_id;
+    public $selected_test_id;
+    public $bal;
+    public $correct_answers_count;
+    public $answers;
+
+
+    public function __construct(public HistoryOfSolution $model, $config = [])
+    {
+        parent::__construct($config);
+    }
+
+    public function save()
+    {
+        $model = $this->model;
+        $model->subject_id = $this->subject_id;
+        $model->test_id = $this->test_id;
+        $model->selected_test_id = $this->selected_test_id;
+        $model->ball = $this->bal;
+        $model->correct_answers_count = $this->correct_answers_count;
+        $model->answers = json_encode($this->answers, JSON_PRETTY_PRINT);
+        return $model->save();
+    }
+}
--- forms/CheckingTestForm.php
+++ forms/CheckingTestForm.php
@@ -10,17 +10,23 @@ namespace app\forms;
 use app\enums\StatusEnum;
 use app\models\Answer;
 use app\models\Question;
+use yii\base\Model;
 
-class CheckingTestForm
+class CheckingTestForm extends Model
 {
     public $count = 0;
+    public $bal = 0;
+
+    public array $answers;
 
     public function __construct(
         public $subject_id,
         public $test_id,
-        public $questions)
+        public $questions,
+               $config = []
+    )
     {
-
+        parent::__construct($config);
     }
 
 
@@ -30,6 +36,8 @@ class CheckingTestForm
         $test_id = $this->test_id;
         $question_ids = $this->questions;
         $count = 0;
+        $bal = 0;
+        $bodyAnswers = [];
         foreach ($question_ids as $question_id => $answer_ids) {
             $answers = Answer::find()
                 ->andWhere(
@@ -43,12 +51,30 @@ class CheckingTestForm
                 ->asArray()
                 ->all();
             $correct_answer_count = 0;
+
+            if (isset($answer_ids[0]) && empty($answer_ids[0])) {
+                unset($answer_ids[0]);
+            }
             foreach ($answer_ids as $id) {
                 if (in_array($id, array_column($answers, 'id'))) {
                     $correct_answer_count++;
                 }
             }
+
+            $questionModel = Question::findOne(['id' => $question_id]);
+
+            $bodyAnswers[$questionModel->id] = [
+                'question' => $questionModel->text,
+                'answers' => $questionModel
+                    ->getAnswers()
+                    ->select(['id', 'text'])
+                    ->asArray()
+                    ->all(),
+                'correct_answer' => $answer_ids,
+            ];
+
             if (count($answers) == $correct_answer_count && $correct_answer_count != 0) {
+                $bal += $questionModel->bal;
                 $count++;
             }
             if ($correct_answer_count == 0) {
@@ -58,13 +84,16 @@ class CheckingTestForm
                     'question_id' => $question_id,
                 ])->text;
                 $answerText = strtolower(strip_tags($answer));
-                $replyText = isset($answer_ids[0]) ? strtolower($answer_ids[0]) : "";
+                $replyText = isset($answer_ids[1]) ? strtolower($answer_ids[1]) : "";
                 if ($answerText == $replyText) {
+                    $bal += $questionModel->bal;
                     $count++;
                 }
             }
         }
         $this->setCorrectCount($count);
+        $this->setBal($bal);
+        $this->setAnswers($bodyAnswers);
         return true;
     }
 
@@ -77,7 +106,7 @@ class CheckingTestForm
         ])->count();
     }
 
-    public function setCorrectCount($correctCount)
+    private function setCorrectCount($correctCount)
     {
         $this->count = $correctCount;
     }
@@ -85,5 +114,25 @@ class CheckingTestForm
     public function getCorrectCount()
     {
         return $this->count;
+    }
+
+    private function setBal($bal)
+    {
+        $this->bal = $bal;
+    }
+
+    public function getBal()
+    {
+        return $this->bal;
+    }
+
+    private function setAnswers($bodyAnswers)
+    {
+        $this->answers = $bodyAnswers;
+    }
+
+    public function getAnswers()
+    {
+        return $this->answers;
     }
 }
