1c29ea747f4667729b889f115148881b3386e4fb
JamshidbekAkhlidinov <jamshidbekakhlidinov@gmail.com>
2023-11-01 12:32:54 +0500

    update view text

--- views/site/about.php
+++ views/site/about.php
@@ -11,8 +11,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Ushbu sayt orqali o'z bilimgizni tekshirib ko'rishingiz mumkun
     </p>
-
-    <code><?= __FILE__ ?></code>
 </div>
--- views/site/index.php
+++ views/site/index.php
@@ -9,14 +9,22 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">O'zingizni fanlardan tekshirib ko'ring!</h1>
+        <h1 class="display-4">
+            O'zingizni fanlardan tekshirib ko'ring!
+        </h1>
 
         <p class="lead">
             Login qiling yoki ro'yxatdan o'ting, Testlarni boshlang va o'z bilim darajadgiz haqida bilib oling.
         </p>
 
-        <?=Html::a("Kirish",['site/login'],['class'=>'btn btn-success'])?>
-        <?=Html::a("Ro'yxatdan o'tish",['site/signup'],['class'=>'btn btn-success'])?>
+        <?= Html::a("Kirish", ['site/login'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a("Ro'yxatdan o'tish", ['site/signup'], ['class' => 'btn btn-success']) ?>
+        <?php if (!user()->isGuest): ?>
+            <div class="pt-3">
+                <?= Html::a("Fanlardan testlar", ['exam/subject'], ['class' => 'btn btn-primary']) ?>
+                <?= Html::a("Tanlanma testlar", ['exam/selected-test'], ['class' => 'btn btn-primary']) ?>
+            </div>
+        <?php endif; ?>
     </div>
 
     <div class="body-content">
